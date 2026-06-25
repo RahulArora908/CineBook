735b940db7f0378333b0cebb6b57d3ed5e2be422
Rahul Arora <rahularora4034@gmail.com>
2026-06-25 17:48:50 +0530

    resolved deployment issues

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { assets } from '../assets/assets'
 import { MenuIcon, SearchIcon, TicketPlus, XIcon } from 'lucide-react'
 import { Show, useClerk, UserButton } from '@clerk/react'
-import { useAppContext } from '../context/appContext'
+import { useAppContext } from '../context/AppContext'
 
 const Navbar = () => {
 
